12eb6b4524b597b4e784d9df70de5d9c118ecc15
SmirnovaAlina <alinas008@gmail.com>
2019-09-18 11:25:55 -0700

    building data pipeline etl.py

--- etl.py
+++ etl.py
@@ -1,3 +1,4 @@
+
 import os
 import glob
 import psycopg2
@@ -7,37 +8,38 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values.tolist()[0]
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values.tolist()[0]
+    
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines = True)
 
     # filter by NextSong action
-    df = 
+    df = df[df.page == "NextSong"]
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df.ts, unit = "ms", origin = 'unix')
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+#     time_data = 
+#     column_labels = 
+    time_df = pd.DataFrame({'start_time' : t.dt.time , 'hour' : t.dt.hour , 'day' : t.dt.day, 'week' : t.dt.week, 'month':t.dt.month, 'year' : t.dt.year, 'weekday' : t.dt.day_name()})
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level' ]]
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -47,11 +49,18 @@ def process_log_file(cur, filepath):
     for index, row in df.iterrows():
         
         # get songid and artistid from song and artist tables
-        results = cur.execute(song_select, (row.song, row.artist, row.length))
-        songid, artistid = results if results else None, None
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
+        
+        starttime = pd.to_datetime(row.ts,unit='ms')
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (index, starttime, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
